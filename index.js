081c5720e3fa11e8b6fa0a4dc4b9daf0e11f97c2
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-12-28 20:12:46 +0530

    user by id

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ const users = require("./MOCK_DATA.json");
 
 //routes
 
-
+//all users
 app.get("/api/users", (req, res) => {
     res.json(users);
   });
 
+//user by id
+app.get("/api/users/:id", (req, res) => {
+    const id=Number(req.params.id);
+    const user=users.find((user)=>user.id===id);
+    res.json(user);
+  });
+
+
+
+//Display first name with html page render
 app.get("/users",(req,res)=>{
     const html=`
     <ul> ${users.map((users)=>`<li>${users.first_name}</li>`).join("")}  </ul>
